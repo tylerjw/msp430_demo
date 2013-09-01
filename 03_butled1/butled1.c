0a43455a3b826824ebbb548350eb250c0dbd2a6f
Tyler Weaver <tylerjw@gmail.com>
2013-09-01 07:29:12 +0430

    03 working

--- 03_butled1/butled1.c
+++ 03_butled1/butled1.c
@@ -5,23 +5,24 @@
 
 #include  <msp430g2553.h>
 
-#define LED_ON  BIT0|BIT6  // both red and green leds
+#define LED_ON  0x41  // both red and green leds
 #define BTN     BIT3       // button 
 
 
 void main (void)
 {
-    WDTCTL = WDTPW | WDTHOLD; // disable wdt+
-    P1DIR |= LED_ON;          // set red and green led out
-    P1REN |= BTN;          // pull enabled
-    P1OUT |= BTN;          // pull up 
+    WDTCTL = WDTPW | WDTHOLD;  // disable wdt+
+    P1DIR |= LED_ON;           // set red and green led out
+    P1REN |= BTN;             // pull enabled
+    P1OUT = BTN;             // pull up 
+
     for(;;) 
     {
-        if(!(P1IN & BTN))     // button is down
+        if((P1IN & BTN) == 0)     // button is down
         {
-            P2OUT |= 1;       // turn the leds on
-        } else {              // button is not pressed
-            P2OUT &= 0;       // turn the leds off
+            P1OUT |= LED_ON;     // turn the leds on
+        } else {                  // button is not pressed
+            P1OUT &= ~LED_ON;      // turn the leds off
         }
     }
 }
